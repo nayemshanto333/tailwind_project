c279e5176fe2e00b76ada7e717b0ea3c974f9fcb
nayemshanto333 <nayemshantobd@gmail.com>
2024-08-24 14:55:01 +0600

    Solved

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
             input: {
                 main: resolve(__dirname, "index.html"),
                 news: resolve(__filename, "news.html"),
-            }
-        }
-    }
-})
+            },
+        },
+    },
+});
