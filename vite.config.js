7a792d185ba35bb012b61d4187cc5c8b43ebb392
nayemshanto333 <nayemshantobd@gmail.com>
2024-08-24 15:17:00 +0600

    solved

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, "index.html"),
-                news: resolve(__filename, "news.html"),
-            },
-        },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        news: resolve(__dirname, 'news.html'),
+      },
     },
-});
+  },
+})
